fcc339649cf8ac312a69c82a7a3ec7a1d5012517
Cybernik12 <niksyn12@yahoo.co.uk>
2022-05-10 10:44:08 +0100

    Changed Project File
    Changed project file, created a character controller, hinge joint and interaction

--- Unity Constraints/Assets/Scripts/Raycast.cs
+++ Unity Constraints/Assets/Scripts/Raycast.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Raycast : MonoBehaviour
 {
-    private Vector3 collision = Vector3.zero;
-    private Rigidbody temp;
+
+    [SerializeField] private GameObject lastHit;
+    [SerializeField] private Vector3 collision = Vector3.zero;
+    [SerializeField] private Rigidbody temp;
+
+    [SerializeField] private float force;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        force = 100f;
+        temp = null;
     }
 
     // Update is called once per frame
@@ -20,20 +25,29 @@ public class Raycast : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit))
         {
+            lastHit = hit.transform.gameObject;
             temp = hit.rigidbody;
             collision = hit.point;
         }
+
+        Interact();
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(collision, 0.2f);
+        Gizmos.DrawWireSphere(collision, 1.0f);
     }
 
-    private void OnMouseDown()
-    { 
-        temp.AddForce(-transform.forward * 500);
-        temp.useGravity = true;
+    private void Interact()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (temp != null)
+            {
+                temp.AddForce(transform.forward * force);
+                temp.useGravity = true;
+            }
+        }
     }
 }
